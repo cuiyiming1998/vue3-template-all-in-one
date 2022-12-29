c4e679b33b411d96d30fc24ad43622e77e667dfe
cuiyiming1998 <1269567664@qq.com>
2022-12-29 18:10:31 +0800

    :rocket: chore(*): 添加lint命令

--- vite.config.ts
+++ vite.config.ts
@@ -8,26 +8,26 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-	plugins: [
-		Pages(),
-		Components({
-			resolvers: [AntDesignVueResolver()]
-		}),
-		AutoImport({
-			imports: ['vue', '@vueuse/core', 'pinia', 'vue-router'],
-			dts: './auto-imports.d.ts',
-			eslintrc: {
-				enabled: false, // Default `false`
-				filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
-				globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
-			}
-		}),
-		vue(),
-		vuejsx()
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src')
-		}
-	}
+  plugins: [
+    Pages(),
+    Components({
+      resolvers: [AntDesignVueResolver()]
+    }),
+    AutoImport({
+      imports: ['vue', '@vueuse/core', 'pinia', 'vue-router'],
+      dts: './auto-imports.d.ts',
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      }
+    }),
+    vue(),
+    vuejsx()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
