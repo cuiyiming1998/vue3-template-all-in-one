568d10586cdf45f3459d48f7bde4a353bd74fec1
cuiyiming1998 <1269567664@qq.com>
2022-12-29 17:18:05 +0800

    :rocket: chore(*): 添加commit-lint

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <router-view></router-view>
 </template>
 
-<style lang="scss">
-#app {
-  height: 100%;
-  width: 100%;
-}
+<style lang="scss" setup>
+  #app {
+    height: 100%;
+    width: 100%;
+  }
 </style>
